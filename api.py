0aaa713f0708155e0d7f7f9774db42f7e229fb73
Paul Karayan <pk@addepar.com>
2014-03-21 14:03:47 -0700

    fucked around with require, now prepping for heroku / flask

---
+++ api.py
@@ -0,0 +1,42 @@
+from flask import Flask
+import json
+
+
+import logging
+from logging import StreamHandler
+
+questions = [{"question":"Who is Prime Minister of the United Kingdom?","choices":["David Cameron","Gordon Brown","Winston Churchill","Tony Blair"],"correctAnswer":0},
+{"question":"What is the landspeed of an African Swallow?","choices":["100km/hr","24mph","5mph","Tony Blair"],"correctAnswer":1},
+{"question":"What is the capital of Slovenia?","choices":["Ljubljana","Maribor","Celje"],"correctAnswer":0}]
+
+def question(qs):
+
+    for item in qs:
+      yield json.dumps(item)
+
+q = question(questions)
+
+file_handler = StreamHandler()
+app = Flask(__name__)
+
+app.logger.setLevel(logging.DEBUG)  # set the desired logging level here
+app.logger.addHandler(file_handler)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/question', methods=['GET'])
+
+# questions = [{"question":"Who is Prime Minister of the United Kingdom?","choices":["David Cameron","Gordon Brown","Winston Churchill","Tony Blair"],"correctAnswer":0},
+# {"question":"What is the landspeed of an African Swallow?","choices":["100km/hr","24mph","5mph","Tony Blair"],"correctAnswer":1},
+# {"question":"What is the capital of Slovenia?","choices":["Ljubljana","Maribor","Celje"],"correctAnswer":0}]
+
+def questionroute():
+  return next(q)
+
+
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ scripts/script.js
@@ -0,0 +1,110 @@
+//provide as JSON. But I want to feed using flask instead so will remove
+
+var allQuestions = [
+{
+    question: "Who is Prime Minister of the United Kingdom?",
+    choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"],
+    correctAnswer: 0
+}, {
+    question: "What is the landspeed of an African Swallow?",
+    choices: ["100km/hr", "24mph", "5mph", "Tony Blair"],
+    correctAnswer: 1
+},
+
+      {question: "What is the capital of Slovenia?",  choices: ["Ljubljana", "Maribor", "Celje"],         correctAnswer: 0}
+];
+
+jsonQ = JSON.stringify(allQuestions);
+
+
+var apiurl = 'http://localhost:5000/question';
+
+function httpGet(URL) {
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", URL, false);
+    xmlHttp.send(null);
+
+    return xmlHttp.responseText;
+  }
+
+console.log(httpGet(apiurl));
+
+
+var questionnum = 0;
+var score = 0;
+var qtext = "Quiz - Question 1";
+var stext = "Score: 0";
+var question = allQuestions[questionnum];
+var choices = question['choices'];
+var answernum = 2;
+
+
+
+function setupQuestions (qnum) {
+
+    console.log(qnum,question,"<-qunum in setupQuestions");
+
+    question = allQuestions[questionnum];
+    $(".question").text(question['question']);
+    choices = question['choices'];
+
+    correct = question['correctAnswer']
+
+    qtext = "Quiz - Question " + (qnum + 1);
+    stext = "Score: " + score;
+
+    $("#qq").text(qtext);
+    $("#score").text(stext);
+
+     console.log(choices, choices.length);
+
+     for (var i = 0; i < choices.length; i++) {
+        var answerinputs = $('<input type="radio" name="answer" value="' + i + '">' + choices[i] + '</input>')
+        $(answerinputs).appendTo('#options');
+    };
+
+};
+
+
+
+
+$(document).ready(function() {
+setupQuestions(0);
+
+$('.scorebut').on('click', function () {
+
+  var answernum = $('input[name=answer]:radio:checked').val()
+
+  //todo - add handling for no checked radio button
+
+    if (correct === parseInt(answernum)) {
+        score = score + 1;
+        console.log(answernum, correct, "score incresase")
+      } else {
+        console.log(answernum, correct, "fail!");
+      };
+
+      $('#options').empty();
+
+
+    console.log(allQuestions.length,questionnum, "nums before setup");
+    questionnum++ ;
+
+
+  if (allQuestions.length === questionnum) {
+
+        $('.wrapper').empty();
+        $('.header').empty();
+
+        var finalscoreoutput = $('<h2> thanks for playing! your score was: ' + score + '</h2>')
+        $(finalscoreoutput).appendTo('.wrapper');
+        var headeroutput = $('<h1> Quiz - Final Score </h2>')
+        $(headeroutput).appendTo('.header');
+    } else {
+
+    setupQuestions(questionnum);
+
+    console.log(allQuestions.length,questionnum, "nums before if");
+  };
+});
+});
